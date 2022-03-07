ed98da92e602be00d1b3e5125106fd80785b117d
JessicaPortilio <you@example.com>
2022-03-07 18:07:44 -0300

    Comportamento de geradores e iterador

--- Comportamento_de_Geradores_e_Iteradores/aula14.py
+++ Comportamento_de_Geradores_e_Iteradores/aula14.py
@@ -8,6 +8,7 @@ for letra in nome:
 print('#' * 20)
 
 iterador = iter(nome)
+
 try:
     print(next(iterador))
     print(next(iterador))
@@ -27,3 +28,28 @@ try:
     print(next(iterador))
 except:
     pass
+
+print('Cadê os Valores')
+for valor in iterador:
+    print(valor)
+print('Eu já consumir os valores usando o next lá em cima')
+
+gerador = (letra for letra in nome)
+
+print(next(gerador))
+print(next(gerador))
+print(next(gerador))
+print(next(gerador))
+print(next(gerador))
+print(next(gerador))
+
+
+
+print('Olha o for')
+for letra in gerador:
+    print(letra)
+
+print('Olha o outro for')
+for letra in gerador:
+    print(letra)
+print('Não tem valor pq já consumiu tudo')
